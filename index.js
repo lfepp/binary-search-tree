57a44a010b7c09977f8126ab50033a17712be721
Luke Epp <lucasfepp@gmail.com>
2016-03-28 12:33:07 -0400

    Added add method

--- index.js
+++ index.js
@@ -15,7 +15,29 @@ binaryTree.prototype = {
       this.root = new Node(val);
       return;
     }
+    var currentNode = root;
+    var newNode = new Node(val);
     // Traverse to find position and add node
+    while(currentNode) {
+      if(val < currentNode.value) {
+        if(!currentNode.left) {
+          currentNode.left = newNode;
+          break;
+        }
+        else {
+          currentNode = currentNode.left;
+        }
+      }
+      else {
+        if(!currentNode.right) {
+          currentNode.right = newNode;
+          break;
+        }
+        else {
+          currentNode = currentNode.right;
+        }
+      }
+    } // while loop
   } // add method
 };
 
