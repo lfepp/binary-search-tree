c4f61c854cfd39edc73c149f312025452eb0ee03
lfepp <lucasfepp@gmail.com>
2016-03-28 21:01:57 -0400

    Added initial test for add method

--- test.js
+++ test.js
@@ -1,1 +1,44 @@
-console.log("You haven't written your tests yet dude. Get your shit together.");
+'use strict';
+
+var binaryTree = require("./");
+var expect = require('chai').expect;
+
+describe('binary tree tests', function() {
+
+  it('adds values to the binary tree', function () {
+    var test = new binaryTree();
+    test.add(7);
+    test.add(43);
+    test.add(13);
+    test.add(27);
+    test.add(82);
+    test.add(2);
+    test.add(19);
+    test.add(8);
+    test.add(1);
+    test.add(92);
+
+    expect(test).to.equal({
+      root:
+       { value: 7,
+         left:
+          { value: 2,
+            left: { value: 1, left: null, right: null },
+            right: null },
+         right:
+          { value: 43,
+            left:
+             { value: 13,
+               left: { value: 8, left: null, right: null },
+               right:
+                { value: 27,
+                  left: { value: 19, left: null, right: null },
+                  right: null } },
+            right:
+             { value: 82,
+               left: null,
+               right: { value: 92, left: null, right: null } } } }
+    });
+  });
+
+});
